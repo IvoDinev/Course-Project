c4be798507894097b2b12fcceac93eee82ce974c
Hristo Popov <201216003@fdiba.tu-sofia.bg>
2019-03-09 15:39:28 +0000

    Fix bugs

--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -4,16 +4,21 @@ import { Subject } from 'rxjs';
 
 export class ShoppingService {
     ingredientsChanged = new EventEmitter<Ingredient[]>();
-    startedEditing = new Subject<number>();  
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
-        new Ingredient('Apples', '5'),
-        new Ingredient('Tomato', '5')
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomato', 5)
     ];
     getIngredients() {
         return this.ingredients.slice();
     }
-    addIngredient(ingredient: Ingredient) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-        this.ingredients.push(ingredient);
+    addIngredient(ingredient: Ingredient) {
+      let searchElement = this.ingredients.find( el => el.name === ingredient.name);
+        if(searchElement){
+          searchElement.amount += ingredient.amount;
+        }else{
+          this.ingredients.push(ingredient);
+        }
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
