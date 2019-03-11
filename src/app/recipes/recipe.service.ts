e63dc0b6acc977152dd666436ce4aeae2722350a
Ivaylo Dinev <201216004@fdiba.tu-sofia.bg>
2019-03-11 18:37:33 +0200

    Added select recipe with routing, Fixed add new recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,19 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'Test', 'https://static01.nyt.com/images/2018/10/18/dining/ch-maple-roast-chicken-horizontal/ch-maple-roast-chicken-horizontal-threeByTwoMediumAt2X-v2.jpg'),
-        new Recipe('New recipe', 'New', 'https://static01.nyt.com/images/2018/10/18/dining/ch-maple-roast-chicken-horizontal/ch-maple-roast-chicken-horizontal-threeByTwoMediumAt2X-v2.jpg')
+        new Recipe('Schnitzel', 'Awesome Schnitzel',
+         'https://static01.nyt.com/images/2018/10/18/dining/ch-maple-roast-chicken-horizontal/ch-maple-roast-chicken-horizontal-threeByTwoMediumAt2X-v2.jpg',
+          [new Ingredient('Meat', 1),
+            new Ingredient('Cheese', 2)
+        ]),
+        new Recipe('Burger', 'Mazen Burger',
+         'https://static01.nyt.com/images/2018/10/18/dining/ch-maple-roast-chicken-horizontal/ch-maple-roast-chicken-horizontal-threeByTwoMediumAt2X-v2.jpg',
+          [new Ingredient('Kjufte', 2),
+            new Ingredient('Sos', 2)])
       ];
     getRecipes() {
         return this.recipes.slice();
